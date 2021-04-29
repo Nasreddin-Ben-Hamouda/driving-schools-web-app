43063ed3fa07832fecc01fbb1520e5adeb93f33c
Nasr Eddine <nasreddinbenhamouda@gmail.com>
2021-04-29 12:00:12 +0100

    implement user office layout and router

---
+++ src/components/backOffices/userOffice/TheContent.js
@@ -0,0 +1,48 @@
+import React, { Suspense } from 'react'
+import {
+  Redirect,
+  Route,
+  Switch
+} from 'react-router-dom'
+import { CContainer, CFade } from '@coreui/react'
+const Dashboard = React.lazy(() => import('../../../views/backOffices/userOffice/Dashboard'));
+
+// routes config
+import routes from '../../../routes/backOffices/userOffice/routes'
+  
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+)
+
+const TheContent = (props) => {
+
+  return (
+    <main className="c-main">
+      <CContainer fluid>
+          <Suspense fallback={loading}>
+              <Switch>
+                  {routes.map((route, idx) => {
+                      return route.component && (
+                          <Route
+                              key={idx}
+                              path={route.path}
+                              exact={route.exact}
+                              name={route.name}
+                              render={props => (
+                                  <CFade>
+                                      <route.component {...props} />
+                                  </CFade>
+                              )} />
+                      )
+                  })}
+                  <Redirect from={props.match.path} to={props.match.path+"/dashboard"} />
+              </Switch>
+          </Suspense>
+      </CContainer>
+    </main>
+  )
+}
+
+export default React.memo(TheContent)
--- src/components/frontOffice/UI/SvgIcon/index.js
+++ src/components/frontOffice/UI/SvgIcon/index.js
@@ -1,5 +1,6 @@
-const SvgIcon = ({ src, width, height }) => (
-        <img src={require(`../../../../assets/frontOffice/img/svg/${src}`).default} alt={src} width={width} height={height}/>
+const SvgIcon = ({ src, width, height,style }) => (
+        <img src={require(`../../../../assets/frontOffice/img/svg/${src}`).default} alt={src} width={width} height={height}
+             style={style}/>
     );
 
 export default SvgIcon;
