6c3ecbf26b99220e4aa52fad7241be5f2f2629d6
Ahmed Benyahia <ahmed.benyahia@dqlick.com>
2021-05-30 16:34:51 +0100

    use and environment var for services uri

--- src/axios/scheduling-service.js
+++ src/axios/scheduling-service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const instance=axios.create({
-    baseURL:"http://localhost:3004"
+    baseURL:process.env.SCHEDULING_SERVICE || "http://localhost:3004"
 });
 
 export default instance;
